51e424467163275ecefe1b55825499be869ed8d2
Tdust <gomes.tdust@gmail.com>
2019-09-18 23:15:50 -0300

    Inicio do projeto com primeiro modelo de tela para levantamento de indices

---
+++ app/src/main/java/com/example/faddigital/IndicesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.faddigital
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IndicesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_indices)
+    }
+}
